be23ec61bb41443f3fe978e099fa80bf63b8cab6
adisonzenemij <adisonzenemij@gmail.com>
2024-04-08 11:19:26 -0500

    Fix: Configuracion Servidor

--- config/server.php
+++ config/server.php
@@ -1,55 +1,157 @@
-<?php    
-    if (isset($_SERVER['MIBDIRS'])) { define('SERVER_MIBDIRS', $_SERVER['MIBDIRS']); }
-    if (isset($_SERVER['MYSQL_HOME'])) { define('SERVER_MYSQL_HOME', $_SERVER['MYSQL_HOME']); }
-    if (isset($_SERVER['OPENSSL_CONF'])) { define('SERVER_OPENSSL_CONF', $_SERVER['OPENSSL_CONF']); }
-    if (isset($_SERVER['PHP_PEAR_SYSCONF_DIR'])) { define('SERVER_PHP_PEAR_SYSCONF_DIR', $_SERVER['PHP_PEAR_SYSCONF_DIR']); }
-    if (isset($_SERVER['PHPRC'])) { define('SERVER_PHPRC', $_SERVER['PHPRC']); }
-    if (isset($_SERVER['TMP'])) { define('SERVER_TMP', $_SERVER['TMP']); }
-    if (isset($_SERVER['HTTP_HOST'])) { define('SERVER_HTTP_HOST', $_SERVER['HTTP_HOST']); }
-    if (isset($_SERVER['HTTP_CONNECTION'])) { define('SERVER_HTTP_CONNECTION', $_SERVER['HTTP_CONNECTION']); }
-    if (isset($_SERVER['HTTP_CACHE_CONTROL'])) { define('SERVER_HTTP_CACHE_CONTROL', $_SERVER['HTTP_CACHE_CONTROL']); }
-    if (isset($_SERVER['HTTP_SEC_CH_UA'])) { define('SERVER_HTTP_SEC_CH_UA', $_SERVER['HTTP_SEC_CH_UA']); }
-    if (isset($_SERVER['HTTP_SEC_CH_UA_MOBILE'])) { define('SERVER_HTTP_SEC_CH_UA_MOBILE', $_SERVER['HTTP_SEC_CH_UA_MOBILE']); }
-    if (isset($_SERVER['HTTP_SEC_CH_UA_PLATFORM'])) { define('SERVER_HTTP_SEC_CH_UA_PLATFORM', $_SERVER['HTTP_SEC_CH_UA_PLATFORM']); }
-    if (isset($_SERVER['HTTP_UPGRADE_INSECURE_REQUESTS'])) { define('SERVER_HTTP_UPGRADE_INSECURE_REQUESTS', $_SERVER['HTTP_UPGRADE_INSECURE_REQUESTS']); }
-    if (isset($_SERVER['HTTP_USER_AGENT'])) { define('SERVER_HTTP_USER_AGENT', $_SERVER['HTTP_USER_AGENT']); }
-    if (isset($_SERVER['HTTP_ACCEPT'])) { define('SERVER_HTTP_ACCEPT', $_SERVER['HTTP_ACCEPT']); }
-    if (isset($_SERVER['HTTP_SEC_FETCH_SITE'])) { define('SERVER_HTTP_SEC_FETCH_SITE', $_SERVER['HTTP_SEC_FETCH_SITE']); }
-    if (isset($_SERVER['HTTP_SEC_FETCH_MODE'])) { define('SERVER_HTTP_SEC_FETCH_MODE', $_SERVER['HTTP_SEC_FETCH_MODE']); }
-    if (isset($_SERVER['HTTP_SEC_FETCH_USER'])) { define('SERVER_HTTP_SEC_FETCH_USER', $_SERVER['HTTP_SEC_FETCH_USER']); }
-    if (isset($_SERVER['HTTP_SEC_FETCH_DEST'])) { define('SERVER_HTTP_SEC_FETCH_DEST', $_SERVER['HTTP_SEC_FETCH_DEST']); }
-    if (isset($_SERVER['HTTP_REFERER'])) { define('SERVER_HTTP_REFERER', $_SERVER['HTTP_REFERER']); }
-    if (isset($_SERVER['HTTP_ACCEPT_ENCODING'])) { define('SERVER_HTTP_ACCEPT_ENCODING', $_SERVER['HTTP_ACCEPT_ENCODING']); }
-    if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) { define('SERVER_HTTP_ACCEPT_LANGUAGE', $_SERVER['HTTP_ACCEPT_LANGUAGE']); }
-    if (isset($_SERVER['HTTP_COOKIE'])) { define('SERVER_HTTP_COOKIE', $_SERVER['HTTP_COOKIE']); }
-    if (isset($_SERVER['PATH'])) { define('SERVER_PATH', $_SERVER['PATH']); }
-    if (isset($_SERVER['SystemRoot'])) { define('SERVER_SystemRoot', $_SERVER['SystemRoot']); }
-    if (isset($_SERVER['COMSPEC'])) { define('SERVER_COMSPEC', $_SERVER['COMSPEC']); }
-    if (isset($_SERVER['PATHEXT'])) { define('SERVER_PATHEXT', $_SERVER['PATHEXT']); }
-    if (isset($_SERVER['WINDIR'])) { define('SERVER_WINDIR', $_SERVER['WINDIR']); }
-    if (isset($_SERVER['SERVER_SIGNATURE'])) { define('SERVER_SERVER_SIGNATURE', $_SERVER['SERVER_SIGNATURE']); }
-    if (isset($_SERVER['SERVER_SOFTWARE'])) { define('SERVER_SERVER_SOFTWARE', $_SERVER['SERVER_SOFTWARE']); }
-    if (isset($_SERVER['SERVER_NAME'])) { define('SERVER_SERVER_NAME', $_SERVER['SERVER_NAME']); }
-    if (isset($_SERVER['SERVER_ADDR'])) { define('SERVER_SERVER_ADDR', $_SERVER['SERVER_ADDR']); }
-    if (isset($_SERVER['SERVER_PORT'])) { define('SERVER_SERVER_PORT', $_SERVER['SERVER_PORT']); }
-    if (isset($_SERVER['REMOTE_ADDR'])) { define('SERVER_REMOTE_ADDR', $_SERVER['REMOTE_ADDR']); }
-    if (isset($_SERVER['DOCUMENT_ROOT'])) { define('SERVER_DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT']); }
-    if (isset($_SERVER['REQUEST_SCHEME'])) { define('SERVER_REQUEST_SCHEME', $_SERVER['REQUEST_SCHEME']); }
-    if (isset($_SERVER['CONTEXT_PREFIX'])) { define('SERVER_CONTEXT_PREFIX', $_SERVER['CONTEXT_PREFIX']); }
-    if (isset($_SERVER['CONTEXT_DOCUMENT_ROOT'])) { define('SERVER_CONTEXT_DOCUMENT_ROOT', $_SERVER['CONTEXT_DOCUMENT_ROOT']); }
-    if (isset($_SERVER['SERVER_ADMIN'])) { define('SERVER_SERVER_ADMIN', $_SERVER['SERVER_ADMIN']); }
-    if (isset($_SERVER['SCRIPT_FILENAME'])) { define('SERVER_SCRIPT_FILENAME', $_SERVER['SCRIPT_FILENAME']); }
-    if (isset($_SERVER['REMOTE_PORT'])) { define('SERVER_REMOTE_PORT', $_SERVER['REMOTE_PORT']); }
-    if (isset($_SERVER['GATEWAY_INTERFACE'])) { define('SERVER_GATEWAY_INTERFACE', $_SERVER['GATEWAY_INTERFACE']); }
-    if (isset($_SERVER['SERVER_PROTOCOL'])) { define('SERVER_SERVER_PROTOCOL', $_SERVER['SERVER_PROTOCOL']); }
-    if (isset($_SERVER['REQUEST_METHOD'])) { define('SERVER_REQUEST_METHOD', $_SERVER['REQUEST_METHOD']); }
-    if (isset($_SERVER['QUERY_STRING'])) { define('SERVER_QUERY_STRING', $_SERVER['QUERY_STRING']); }
-    if (isset($_SERVER['REQUEST_URI'])) { define('SERVER_REQUEST_URI', $_SERVER['REQUEST_URI']); }
-    if (isset($_SERVER['SCRIPT_NAME'])) { define('SERVER_SCRIPT_NAME', $_SERVER['SCRIPT_NAME']); }
-    if (isset($_SERVER['PHP_SELF'])) { define('SERVER_PHP_SELF', $_SERVER['PHP_SELF']); }
-    if (isset($_SERVER['REQUEST_TIME_FLOAT'])) { define('SERVER_REQUEST_TIME_FLOAT', $_SERVER['REQUEST_TIME_FLOAT']); }
-    if (isset($_SERVER['REQUEST_TIME'])) { define('SERVER_REQUEST_TIME', $_SERVER['REQUEST_TIME']); }
-    if (isset($_SERVER['HTTPS'])) { define('SERVER_HTTPS', $_SERVER['HTTPS']); }
+<?php
+    if (isset($_SERVER['MIBDIRS'])) {
+        define('SERVER_MIBDIRS', $_SERVER['MIBDIRS']);
+    }
+    if (isset($_SERVER['MYSQL_HOME'])) {
+        define('SERVER_MYSQL_HOME', $_SERVER['MYSQL_HOME']);
+    }
+    if (isset($_SERVER['OPENSSL_CONF'])) {
+        define('SERVER_OPENSSL_CONF', $_SERVER['OPENSSL_CONF']);
+    }
+    if (isset($_SERVER['PHP_PEAR_SYSCONF_DIR'])) {
+        define('SERVER_PHP_PEAR_SYSCONF_DIR', $_SERVER['PHP_PEAR_SYSCONF_DIR']);
+    }
+    if (isset($_SERVER['PHPRC'])) {
+        define('SERVER_PHPRC', $_SERVER['PHPRC']);
+    }
+    if (isset($_SERVER['TMP'])) {
+        define('SERVER_TMP', $_SERVER['TMP']);
+    }
+    if (isset($_SERVER['HTTP_HOST'])) {
+        define('SERVER_HTTP_HOST', $_SERVER['HTTP_HOST']);
+    }
+    if (isset($_SERVER['HTTP_CONNECTION'])) {
+        define('SERVER_HTTP_CONNECTION', $_SERVER['HTTP_CONNECTION']);
+    }
+    if (isset($_SERVER['HTTP_CACHE_CONTROL'])) {
+        define('SERVER_HTTP_CACHE_CONTROL', $_SERVER['HTTP_CACHE_CONTROL']);
+    }
+    if (isset($_SERVER['HTTP_SEC_CH_UA'])) {
+        define('SERVER_HTTP_SEC_CH_UA', $_SERVER['HTTP_SEC_CH_UA']);
+    }
+    if (isset($_SERVER['HTTP_SEC_CH_UA_MOBILE'])) {
+        define('SERVER_HTTP_SEC_CH_UA_MOBILE', $_SERVER['HTTP_SEC_CH_UA_MOBILE']);
+    }
+    if (isset($_SERVER['HTTP_SEC_CH_UA_PLATFORM'])) {
+        define('SERVER_HTTP_SEC_CH_UA_PLATFORM', $_SERVER['HTTP_SEC_CH_UA_PLATFORM']);
+    }
+    if (isset($_SERVER['HTTP_UPGRADE_INSECURE_REQUESTS'])) {
+        define('SERVER_HTTP_UPGRADE_INSECURE_REQUESTS', $_SERVER['HTTP_UPGRADE_INSECURE_REQUESTS']);
+    }
+    if (isset($_SERVER['HTTP_USER_AGENT'])) {
+        define('SERVER_HTTP_USER_AGENT', $_SERVER['HTTP_USER_AGENT']);
+    }
+    if (isset($_SERVER['HTTP_ACCEPT'])) {
+        define('SERVER_HTTP_ACCEPT', $_SERVER['HTTP_ACCEPT']);
+    }
+    if (isset($_SERVER['HTTP_SEC_FETCH_SITE'])) {
+        define('SERVER_HTTP_SEC_FETCH_SITE', $_SERVER['HTTP_SEC_FETCH_SITE']);
+    }
+    if (isset($_SERVER['HTTP_SEC_FETCH_MODE'])) {
+        define('SERVER_HTTP_SEC_FETCH_MODE', $_SERVER['HTTP_SEC_FETCH_MODE']);
+    }
+    if (isset($_SERVER['HTTP_SEC_FETCH_USER'])) {
+        define('SERVER_HTTP_SEC_FETCH_USER', $_SERVER['HTTP_SEC_FETCH_USER']);
+    }
+    if (isset($_SERVER['HTTP_SEC_FETCH_DEST'])) {
+        define('SERVER_HTTP_SEC_FETCH_DEST', $_SERVER['HTTP_SEC_FETCH_DEST']);
+    }
+    if (isset($_SERVER['HTTP_REFERER'])) {
+        define('SERVER_HTTP_REFERER', $_SERVER['HTTP_REFERER']);
+    }
+    if (isset($_SERVER['HTTP_ACCEPT_ENCODING'])) {
+        define('SERVER_HTTP_ACCEPT_ENCODING', $_SERVER['HTTP_ACCEPT_ENCODING']);
+    }
+    if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+        define('SERVER_HTTP_ACCEPT_LANGUAGE', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+    }
+    if (isset($_SERVER['HTTP_COOKIE'])) {
+        define('SERVER_HTTP_COOKIE', $_SERVER['HTTP_COOKIE']);
+    }
+    if (isset($_SERVER['PATH'])) {
+        define('SERVER_PATH', $_SERVER['PATH']);
+    }
+    if (isset($_SERVER['SystemRoot'])) {
+        define('SERVER_SYSTEMROOT', $_SERVER['SystemRoot']);
+    }
+    if (isset($_SERVER['COMSPEC'])) {
+        define('SERVER_COMSPEC', $_SERVER['COMSPEC']);
+    }
+    if (isset($_SERVER['PATHEXT'])) {
+        define('SERVER_PATHEXT', $_SERVER['PATHEXT']);
+    }
+    if (isset($_SERVER['WINDIR'])) {
+        define('SERVER_WINDIR', $_SERVER['WINDIR']);
+    }
+    if (isset($_SERVER['SERVER_SIGNATURE'])) {
+        define('SERVER_SERVER_SIGNATURE', $_SERVER['SERVER_SIGNATURE']);
+    }
+    if (isset($_SERVER['SERVER_SOFTWARE'])) {
+        define('SERVER_SERVER_SOFTWARE', $_SERVER['SERVER_SOFTWARE']);
+    }
+    if (isset($_SERVER['SERVER_NAME'])) {
+        define('SERVER_SERVER_NAME', $_SERVER['SERVER_NAME']);
+    }
+    if (isset($_SERVER['SERVER_ADDR'])) {
+        define('SERVER_SERVER_ADDR', $_SERVER['SERVER_ADDR']);
+    }
+    if (isset($_SERVER['SERVER_PORT'])) {
+        define('SERVER_SERVER_PORT', $_SERVER['SERVER_PORT']);
+    }
+    if (isset($_SERVER['REMOTE_ADDR'])) {
+        define('SERVER_REMOTE_ADDR', $_SERVER['REMOTE_ADDR']);
+    }
+    if (isset($_SERVER['DOCUMENT_ROOT'])) {
+        define('SERVER_DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT']);
+    }
+    if (isset($_SERVER['REQUEST_SCHEME'])) {
+        define('SERVER_REQUEST_SCHEME', $_SERVER['REQUEST_SCHEME']);
+    }
+    if (isset($_SERVER['CONTEXT_PREFIX'])) {
+        define('SERVER_CONTEXT_PREFIX', $_SERVER['CONTEXT_PREFIX']);
+    }
+    if (isset($_SERVER['CONTEXT_DOCUMENT_ROOT'])) {
+        define('SERVER_CONTEXT_DOCUMENT_ROOT', $_SERVER['CONTEXT_DOCUMENT_ROOT']);
+    }
+    if (isset($_SERVER['SERVER_ADMIN'])) {
+        define('SERVER_SERVER_ADMIN', $_SERVER['SERVER_ADMIN']);
+    }
+    if (isset($_SERVER['SCRIPT_FILENAME'])) {
+        define('SERVER_SCRIPT_FILENAME', $_SERVER['SCRIPT_FILENAME']);
+    }
+    if (isset($_SERVER['REMOTE_PORT'])) {
+        define('SERVER_REMOTE_PORT', $_SERVER['REMOTE_PORT']);
+    }
+    if (isset($_SERVER['GATEWAY_INTERFACE'])) {
+        define('SERVER_GATEWAY_INTERFACE', $_SERVER['GATEWAY_INTERFACE']);
+    }
+    if (isset($_SERVER['SERVER_PROTOCOL'])) {
+        define('SERVER_SERVER_PROTOCOL', $_SERVER['SERVER_PROTOCOL']);
+    }
+    if (isset($_SERVER['REQUEST_METHOD'])) {
+        define('SERVER_REQUEST_METHOD', $_SERVER['REQUEST_METHOD']);
+    }
+    if (isset($_SERVER['QUERY_STRING'])) {
+        define('SERVER_QUERY_STRING', $_SERVER['QUERY_STRING']);
+    }
+    if (isset($_SERVER['REQUEST_URI'])) {
+        define('SERVER_REQUEST_URI', $_SERVER['REQUEST_URI']);
+    }
+    if (isset($_SERVER['SCRIPT_NAME'])) {
+        define('SERVER_SCRIPT_NAME', $_SERVER['SCRIPT_NAME']);
+    }
+    if (isset($_SERVER['PHP_SELF'])) {
+        define('SERVER_PHP_SELF', $_SERVER['PHP_SELF']);
+    }
+    if (isset($_SERVER['REQUEST_TIME_FLOAT'])) {
+        define('SERVER_REQUEST_TIME_FLOAT', $_SERVER['REQUEST_TIME_FLOAT']);
+    }
+    if (isset($_SERVER['REQUEST_TIME'])) {
+        define('SERVER_REQUEST_TIME', $_SERVER['REQUEST_TIME']);
+    }
+    if (isset($_SERVER['HTTPS'])) {
+        define('SERVER_HTTPS', $_SERVER['HTTPS']);
+    }
 
     $serverIsHttps = !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off';
     $serverScheme = $serverIsHttps ? 'https' : 'http';
@@ -57,4 +159,3 @@
     $serverUrl = $serverScheme . '://' . $serverHost . SERVER_REQUEST_URI;
 
     define('SERVER_URL_CURRENT', $serverUrl);
-?>
